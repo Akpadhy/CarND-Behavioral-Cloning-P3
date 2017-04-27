652cc599df6b3a030a5334a67be40847ef7456e4
tulasiram <tulasiram.gunipati@gmail.com>
2017-04-27 22:22:03 +0530

    Add submission files

--- drive.py
+++ drive.py
@@ -60,6 +60,7 @@ def telemetry(sid, data):
         # The current image from the center camera of the car
         imgString = data["image"]
         image = Image.open(BytesIO(base64.b64decode(imgString)))
+        image = image.resize((200,96),Image.ANTIALIAS)
         image_array = np.asarray(image)
         steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
 
